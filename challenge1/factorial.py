3082feea51888160550cd463aa1d8d0af57a8faa
sharan <139887231+sharann16@users.noreply.github.com>
2023-09-08 20:08:55 +0530

    Delete challenge1 directory

--- challenge1/factorial.py
+++
@@ -1,9 +0,0 @@
-def  factorial(n):
-  if n==0|n==1:
-    return 1
-  else:
-    return n*factorial(n-1)
-number=5
-result=factorial(number)
-
-print("the facti=orial of {}is{}.".format(number,result))
